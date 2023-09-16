5d399a0d8e9b033e831e2c7942f1246159dc3a9a
ouyangemma <113309816+ouyangemma@users.noreply.github.com>
2023-09-16 19:05:07 +0000

    Created main window and popup window frame

--- emma.py
+++ emma.py
@@ -1,6 +1,37 @@
+from tkinter import *
+from tkinter import ttk
 
-import tkinter as tk
+# creating the main application window
+window = Tk()
+window.title("PATIENT ALERT")
+window.geometry("500x400")
 
-main = tk.Tk()
+# creating a frame for the main label
+# Create a canvas widget to draw the rectangle
+canvas = Canvas(window, width=300, height=200)
+canvas.pack()
 
-main.mainloop()
+# Create a red rectangle on the canvas
+rectangle = canvas.create_rectangle(50, 100, 250, 200, fill= '#e74c3c')
+
+# Add text on top of the rectangle
+text = canvas.create_text(150, 150, text="Abnormal Value Detected!", fill="white", font=("Helvetica", 16))
+
+# creating label widgets (substituting this with text on a rectangle)
+# outlier_label = ttk.Label(window, text = "Out of Bounds Value Detected")
+# outlier_label.place(x = 40, y = 60)
+
+# function to display more information
+def openPatientWindow():
+    # Toplevel object will be treated as a new window
+    newWindow = Toplevel(window)
+    newWindow.title("Patient Profile")
+    newWindow.geometry("500x400")
+
+
+# "view patient" button
+patient_button = ttk.Button(window, text = "View Patient Information", command = openPatientWindow)
+patient_button.place(x = 150, y = 250)
+
+
+window.mainloop()

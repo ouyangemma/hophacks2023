97864ca31666bdae505ebb3d0bb627220f0b02f5
ouyangemma <113309816+ouyangemma@users.noreply.github.com>
2023-09-16 02:52:24 +0000

    rage

--- emma.py
+++ emma.py
@@ -1,26 +1,6 @@
 
 import tkinter as tk
-import sys
-import os
 
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
+main = tk.Tk()
 
-
-#create main window
-master = tk.Tk()
-master.title("tester")
-master.geometry("300x100")
-
-
-#make a label for the window
-label1 = tk.Label(master, text='Hellooooo')
-# Lay out label
-label1.pack()
-
-# Run forever!
-master.mainloop()
-
-
-# window = tk.Tk()
+main.mainloop()
